35a53a439ca2397202b4fccbbf9d6cb46369f5a4
Silvia Gonzalves <sgonzal0@hotmail.com>
2022-10-20 11:15:34 -0300

    desafio clase2

---
+++ desafio-clase-2.js
@@ -0,0 +1,59 @@
+
+
+
+class Usuario {
+    constructor (nombre,apellido, libros, mascotas){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = libros;
+        this.mascotas = mascotas;
+    }
+    getFullName(){
+        console.log (`Nombre completo:  ${this.nombre} ${this.apellido}`)
+    }
+    addMascotas = (masc) => {
+        this.mascotas.push(masc)
+    }
+    countMascotas(){
+        console.log(this.mascotas.length)
+    }
+    addBook = (lib) => {
+        this.libros.push(lib)
+    }
+    getBookNames(){
+        this.libros.forEach(element => {
+            console.log(`${element.nombre}`)
+        });
+
+
+    }
+}
+
+const usuario = new Usuario("Silvia","Gonzalves",
+                            [{nombre : "Libro 1",
+                              autor : "Autor 1"
+                            },
+                            {nombre : "Libro 2",
+                             autor : "Autor 2"
+                            },
+                            {nombre : "Libro 3",
+                             autor : "Autor 3"
+                            },
+                            {nombre : "Libro 4",
+                             autor : "Autor 4"
+                            }],
+                            ["perro", "gato", "pez", "ardillas"]
+                            )
+
+console.log(usuario)
+usuario.getFullName()
+
+usuario.addMascotas("loro")
+console.log(usuario)
+
+usuario.countMascotas()
+
+usuario.addBook({nombre : "Libro 5", autor : "Autor 5"})
+console.log(usuario)
+
+usuario.getBookNames()
